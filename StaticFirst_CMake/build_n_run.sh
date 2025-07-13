6b999ec604dd58e460b80c583a4028778e20fbc5
carlynorama <carlynorama@gmail.com>
2025-07-13 13:35:47 -0700

    links to build folder

--- StaticFirst_CMake/build_n_run.sh
+++ StaticFirst_CMake/build_n_run.sh
@@ -3,29 +3,32 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPOROOT=$(git rev-parse --show-toplevel)
 SRCROOT=$SCRIPT_DIR
-BUILDROOT=$SCRIPT_DIR/.build
 
-PROJECT=hello
-EXPECTED_EXECUTABLE=$BUILDROOT/$PROJECT
+SRC_LIB_DIR=$SRCROOT/LibraryProject
+BUILD_LIB_DIR=$SRC_LIB_DIR/build
+NAME_LIB="MyFancyResponseHandler"
+export LIBRARY_NAME=$NAME_LIB
+export EXAMP_LIB_INSTALL_DIR=$BUILD_LIB_DIR/lib_src
 
-export PROJECT_NAME=$PROJECT
+SRC_EXE_DIR=$SRCROOT/MainProject
+BUILD_EXE_DIR=$SRC_EXE_DIR/build
+NAME_EXE="hello"
+export EXE_TARGET_NAME=$NAME_EXE
+ #$SCRIPT_DIR/MyFancyResponderLib
 
-export LIBRARY_EXPORT_DIR=$SCRIPT_DIR/MyFancyResponderLib
+#### THE LIBRARY BUILD ####
+cd $SRC_LIB_DIR
+mkdir -p $BUILD_LIB_DIR
+cmake -B $BUILD_LIB_DIR -G Ninja .
+ninja -C $BUILD_LIB_DIR $NAME_LIB
+cd ..
 
-#### THE BUILD ####
-mkdir -p $BUILDROOT
-
-## Must use ninja or xcode as generators if using swift mode
-cmake -B $BUILDROOT -G Ninja .
-
-## OPTION 1
-## let cmake build what ninja generated
-# cmake --build $BUILDROOT
-
-## OPTION 2
-# alternate to below, build with Ninja
-ninja -C $BUILDROOT $PROJECT 
-
-cd $BUILDROOT/hello_src
-./$PROJECT
+#### THE EXECUTABLE BUILD ####
+cd $SRC_EXE_DIR
+mkdir -p $BUILD_EXE_DIR
+cmake -B $BUILD_EXE_DIR -G Ninja .
+ninja -C $BUILD_EXE_DIR $NAME_EXE
+# echo | pwd
+cd $BUILD_EXE_DIR/hello_src/
+./$NAME_EXE
 cd ..
